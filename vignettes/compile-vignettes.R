479d0157d2706d40db0463626bd32617c681142f
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2020-07-14 13:40:57 -0400

    precompile the vignette

---
+++ vignettes/compile-vignettes.R
@@ -0,0 +1,4 @@
+# based on advice from rOpenSci https://ropensci.org/technotes/2019/12/08/precompute-vignettes/
+
+library(knitr)
+knit("vignettes/telecharge-sites.Rmd.orig", "vignettes/telecharge-sites.Rmd")
