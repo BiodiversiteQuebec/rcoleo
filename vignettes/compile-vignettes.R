8b6bbeee95dd927762d5a4e170c702b703d02b44
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2020-07-14 22:33:25 -0400

    it matters where the .httr token is, because apparenlty the wd moves

--- vignettes/compile-vignettes.R
+++ vignettes/compile-vignettes.R
@@ -1,6 +1,6 @@
 # based on advice from rOpenSci https://ropensci.org/technotes/2019/12/08/precompute-vignettes/
 
 library(knitr)
-knit("vignettes/telecharge-sites.Rmd.orig", "vignettes/telecharge-sites.Rmd")
-
 knit("vignettes/injection-taxa.Rmd.orig", "vignettes/injection-taxa.Rmd")
+
+knit("vignettes/telecharge-sites.Rmd.orig", "vignettes/telecharge-sites.Rmd")
