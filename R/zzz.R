716a90eed115aecdb3e32c1283ecc0283d44234b
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2020-08-20 11:21:54 -0400

    no queries happening in rcoleo! at least not at this time

--- R/zzz.R
+++ R/zzz.R
@@ -38,20 +38,3 @@ msg_request_fail <- function(resp) {
   warning("API request failed: error ", sta)
 }
 
-
-# handle_query <- function(query, names_available) {
-#   if (is.character(query)) return(list(q = query))
-#   if (!is.list(query))
-#     stop("`query` should either be a list or a character string.",
-#          call. = FALSE)
-#   if (length(query) > 1) {
-#     warning("Only the first element of the list is considered.", call. = FALSE)
-#     query <- query[1L]
-#   }
-#   if (! names(query) %in% names_available)
-#     stop("Only ", paste(names_available, collapse = ", "),
-#          " are valid names for custom queries.", call. = FALSE)
-#   query
-# }
-# # Remove ==> other message "should be named"
-#
