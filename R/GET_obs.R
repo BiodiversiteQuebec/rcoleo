50a28291e78e8d8aa97106221a7ecc9afeaa7ffa
Sckende <clrccljuhasz@gmail.com>
2020-09-23 13:48:31 -0400

    # New get_obs() - bug partiellement fixé

--- R/GET_obs.R
+++ R/GET_obs.R
@@ -7,149 +7,123 @@
 #' \dontrun{
 #' get_obs()
 #' get_obs(type = 'mammifères')
-#' } 
+#' }
 #' @export
 
 # TODO: ne couvre pas les observations de sol
 
-get_obs <- function(site_code = NULL, opened_at = NULL, closed_at = NULL, type = NULL, ...) {
-
+get_obs <- function (site_code = NULL, opened_at = NULL, closed_at = NULL,
+                       type = NULL, ...)
+{
   endpoint <- endpoints()$observations
-  token <- ifelse(is.na(bearer()),list(...)$token ,bearer())
-
-  # Preparation de l'objet de sortie
+  token <- ifelse(is.na(bearer()), list(...)$token, bearer())
   responses <- list()
   class(responses) <- "coleoGetResp"
 
-  # Si tous les arguments sont nuls
-  if(all(is.null(site_code), is.null(opened_at), is.null(closed_at), is.null(type))){
-
+  # ----------------------------------------------------------------- #
+  if (all(is.null(site_code), is.null(opened_at), is.null(closed_at),
+          is.null(type))) {
     responses[[1]] <- get_gen(endpoint, ...)
-
-    # campaign ids
-    responses[[1]] <- lapply(responses[[1]], function(page){
-
-      # On ajoute les informations des campagnes
-      campaign_ids <- unique(page$body$campaign_id)
+    responses[[1]] <- lapply(responses[[1]]$body, function(page) {
+      campaign_ids <- unique(page$campaign_id)
       campaigns_info <- list()
+      for (i in 1:length(campaign_ids)) {
+        campaign <- httr::content(httr::GET(url = paste0(server(),
+                                                         "/api/v1/campaigns/", campaign_ids[i]),
+                                            config = httr::add_headers(`Content-type` = "application/json",
+                                                                       Authorization = paste("Bearer", token)),
+                                            ua), simplify = TRUE)
 
-      for(i in 1:length(campaign_ids)){
-        campaign <- httr::content(httr::GET(url=paste0(server(),"/api/v1/campaigns/",campaign_ids[i]), config = httr::add_headers(`Content-type` = "application/json",Authorization = paste("Bearer",token)),ua), simplify = TRUE)
-        if(is.null(campaign$closed_at)) campaign$closed_at <- NA
-        campaigns_info[[i]] <- data.frame(campaign_id = campaign$id, site_id=campaign$site_id, opened_at=campaign$opened_at, closed_at=campaign$closed_at, type=campaign$type)
+        if (is.null(campaign$closed_at))
+          campaign$closed_at <- NA
+        campaigns_info[[i]] <- data.frame(campaign_id = campaign$id,
+                                          site_id = campaign$site_id, opened_at = campaign$opened_at,
+                                          closed_at = campaign$closed_at, type = campaign$type)
       }
 
-      campaigns_info <- do.call(plyr::rbind.fill,campaigns_info)
-      page$body <- merge(page$body,campaigns_info, by="campaign_id")
-
-      # On ajoute les informations du site
-      site_ids <- unique(page$body$site_id)
+      campaigns_info <- do.call(plyr::rbind.fill, campaigns_info)
+      page <- merge(page, campaigns_info, by = "campaign_id")
+      site_ids <- unique(page$site_id)
       sites_info <- list()
 
-      for(i in 1:length(site_ids)){
-        site <- httr::content(httr::GET(url=paste0(server(),"/api/v1/sites/",site_ids[i]), config = httr::add_headers(`Content-type` = "application/json",Authorization = paste("Bearer", token)),ua), simplify = TRUE)
-        sites_info[[i]] <- data.frame(site_id = site$id, site_code = site$site_code, cell_code = site$cell$cell_code)
+      for (i in 1:length(site_ids)) {
+        site <- httr::content(httr::GET(url = paste0(server(),
+                                                     "/api/v1/sites/", site_ids[i]), config = httr::add_headers(`Content-type` = "application/json",
+                                                                                                                Authorization = paste("Bearer", token)),
+                                        ua), simplify = TRUE)
+        sites_info[[i]] <- data.frame(site_id = site$id,
+                                      site_code = site$site_code, cell_code = site$cell$cell_code)
       }
-
-      sites_info <- do.call(plyr::rbind.fill,sites_info)
-      page$body <- merge(page$body,sites_info, by="site_id")
-
-      # On restructure le data.frame pour la sortie
-      page$body <- dplyr::select(page$body,
-        "id",
-        "cell_code",
-        "site_code",
-        "opened_at",
-        "closed_at",
-        "type",
-        "axis",
-        "distance",
-        "distance_unit",
-        "sample_id",
-        "date_obs",
-        "obs_species.taxa_name",
-        "obs_species.variable",
-        "obs_species.value",
-        "is_valid",
-        "media",
-        "notes",
-        "created_at",
-        "updated_at"
-      )
-
+      sites_info <- do.call(plyr::rbind.fill, sites_info)
+      page <- merge(page, sites_info, by = "site_id")
+      page <- dplyr::select(page, "id",
+                            "cell_code", "site_code", "opened_at",
+                            "closed_at", "type", "axis",
+                            "distance", "distance_unit", "sample_id",
+                            "date_obs", "obs_species.taxa_name",
+                            "obs_species.variable", "obs_species.value",
+                            "is_valid", "media", "notes",
+                            "created_at", "updated_at")
       return(page)
     })
 
-
   } else {
+    #browser()
+    len <- max(c(length(site_code), length(opened_at), length(closed_at),
+                 length(type)))
 
-    # tests args to set iterator
-    len <- max(c(length(site_code),length(opened_at),length(closed_at),length(type)))
+    for (r in 1:len) {
+      #browser()
+      cc <- rcoleo::get_campaigns(site_code = site_code[r],
+                                  opened_at = opened_at[r],
+                                  closed_at = closed_at[r],
+                                  type = type[r])
+      # cc <- get_campaigns_2(site_code = site_code[r],
+      #                             opened_at = opened_at[r],
+      #                             closed_at = closed_at[r],
+      #                             type = type[r])
 
-    # Loop sur les campagnes demandees
-    for(r in 1:len){
-      campaigns_ids <- as.data.frame(get_campaigns(site_code=site_code[r],
-                    opened_at = opened_at[r],
-                    closed_at = closed_at[r],
-                    type = type[r]))$id
+      # sortie similaire avec rcoleo::get_campaigns & get_campaigns_2
+      ccc <- lapply(cc, function(x) x$body[[1]]$id)
+      campaigns_ids <- unlist(ccc)
     }
-
     # On récupère les observations pour la campagne concernée
-    for(i in 1:length(campaigns_ids)) responses[[i]] <- get_gen(endpoint, query=list(campaign_id=campaigns_ids[i]), ...)
-
-    # TODO: Fonction d'extend plus generic
-    responses <- lapply(responses, function(response){
-
-      lapply(response, function(page) {
-
-        # si la reponse contient quelque chose
-        campaign_id <- unique(page$body$campaign_id)
-
-        # on verifie que l'on a bien une seule campagne a l'interieur du body
-        stopifnot(length(campaign_id) == 1)
-
-        # Campagne info
-        campaign_info <- httr::content(httr::GET(url=paste0(server(),"/api/v1/campaigns/",campaign_id), config = httr::add_headers(`Content-type` = "application/json",Authorization = paste("Bearer", ifelse(is.na(bearer()),token,bearer()))),ua), simplify = TRUE)
-        # Code du site
-        site_info <- httr::content(httr::GET(url=paste0(server(),"/api/v1/sites/",campaign_info$site_id), config = httr::add_headers(`Content-type` = "application/json",Authorization = paste("Bearer", ifelse(is.na(bearer()),token,bearer()))),ua), simplify = TRUE)
-
-        # On ajoute les informations de la campagne
-        page$body$site_code <- site_info$site_code
-        page$body$cell_code <- site_info$cell$cell_code
-        page$body$opened_at <- campaign_info$opened_at
-        page$body$closed_at <- campaign_info$closed_at
-        page$body$type <- campaign_info$type
-
-        # On restructure le data.frame pour la sortie
-        page$body <- dplyr::select(page$body,
-          "id",
-          "cell_code",
-          "site_code",
-          "opened_at",
-          "closed_at",
-          "type",
-          "axis",
-          "distance",
-          "distance_unit",
-          "sample_id",
-          "date_obs",
-          "obs_species.taxa_name",
-          "obs_species.variable",
-          "obs_species.value",
-          "is_valid",
-          "media",
-          "notes",
-          "created_at",
-          "updated_at"
-        )
-
-        return(page)
-
+    for (i in 1:length(campaigns_ids)) responses[[i]] <- get_gen(endpoint,
+                                                                 query = list(campaign_id = campaigns_ids[i]), ...)
+    if(FALSE){
+      # TODO: Fonction d'extend plus generic
+      responses <- lapply(responses, function(response) {
+        lapply(response, function(page) {
+          campaign_id <- unique(page$campaign_id)
+          stopifnot(length(campaign_id) == 1)
+          campaign_info <- httr::content(httr::GET(url = paste0(server(),
+                                                                "/api/v1/campaigns/",
+                                                                campaign_id),
+                                                   config = httr::add_headers(`Content-type` = "application/json",
+                                                                              Authorization = paste("Bearer", ifelse(is.na(bearer()),
+                                                                                                                     token, bearer()))), ua), simplify = TRUE)
+          site_info <- httr::content(httr::GET(url = paste0(server(),
+                                                            "/api/v1/sites/", campaign_info$site_id),
+                                               config = httr::add_headers(`Content-type` = "application/json",
+                                                                          Authorization = paste("Bearer", ifelse(is.na(bearer()),
+                                                                                                                 token, bearer()))), ua), simplify = TRUE)
+          page$site_code <- site_info$site_code
+          page$cell_code <- site_info$cell$cell_code
+          page$opened_at <- campaign_info$opened_at
+          page$closed_at <- campaign_info$closed_at
+          page$type <- campaign_info$type
+          page <- dplyr::select(page, "id",
+                                "cell_code", "site_code", "opened_at",
+                                "closed_at", "type", "axis",
+                                "distance", "distance_unit", "sample_id",
+                                "date_obs", "obs_species.taxa_name",
+                                "obs_species.variable", "obs_species.value",
+                                "is_valid", "media", "notes",
+                                "created_at", "updated_at")
+          return(page)
+        })
       })
-
-    })
+    }
   }
-
   return(responses)
-
 }
