533b2324a95fc44321b33ebc12cc1d5e9bcd615b
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2020-11-02 12:35:57 -0500

    vignette creation -- use different format

--- vignettes/dev-vignettes.R
+++ vignettes/dev-vignettes.R
@@ -1,8 +1,10 @@
 # based on advice from rOpenSci https://ropensci.org/technotes/2019/12/08/precompute-vignettes/
 
+
+
 library(knitr)
 knit("vignettes/injection-taxa.Rmd.orig", "vignettes/injection-taxa.Rmd")
 
 knit("vignettes/telecharge-sites.Rmd.orig", "vignettes/telecharge-sites.Rmd")
 
-knit("vignettes/tutoriel.Rmd.orig", "vignettes/tutoriel.Rmd`")
+knit("vignettes/tutoriel.Rmd.orig", "vignettes/tutoriel.Rmd")
