81c90c1322d35f21f09696dfe6f3d3c797f3c4e7
Sckende <clrccljuhasz@gmail.com>
2020-11-12 10:21:32 -0500

    # Organisation des vignettes
    # Ajout des nouvelles vignettes sur le site (section `Articles)`

--- vignettes/compile-vignettes.R
+++ vignettes/compile-vignettes.R
@@ -1,8 +1,18 @@
 # based on advice from rOpenSci https://ropensci.org/technotes/2019/12/08/precompute-vignettes/
 
 library(knitr)
-knit("vignettes/injection-taxa.Rmd.orig", "vignettes/injection-taxa.Rmd")
-
-knit("vignettes/telecharge-sites.Rmd.orig", "vignettes/telecharge-sites.Rmd")
 
 knit("vignettes/tutoriel.Rmd.orig", "vignettes/tutoriel.Rmd`")
+knit("vignettes/tutoriel-injections.Rmd.orig", "vignettes/tutoriel-injections.Rmd`")
+
+# Téléchargement
+knit("vignettes/telecharge-sites.Rmd.orig", "vignettes/telecharge-sites.Rmd")
+
+# Injections TABLES
+knit("vignettes/injection-taxa.Rmd.orig", "vignettes/injection-taxa.Rmd")
+knit("vignettes/injection-cellules.Rmd.orig", "vignettes/injection-cellules.Rmd")
+knit("vignettes/injection-sites.Rmd.orig", "vignettes/injection-sites.Rmd")
+
+# Injections CAMPAGNES
+knit("vignettes/CAMPAGNE_zooplancton_injection.Rmd.orig", "vignettes/CAMPAGNE_zooplancton_injection.Rmd")
+
