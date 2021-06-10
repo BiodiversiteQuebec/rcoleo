a9427d6159e5140a6d6464d486c14f2cabd91185
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2021-06-10 15:27:50 -0400

    get_species_list assert and test that inputs are not both NULL

--- R/species_by_site.R
+++ R/species_by_site.R
@@ -4,11 +4,17 @@
 #' get the species list for a site
 #'
 #' @param campaign_type campaign type. Can be any valid campaign name, see [validate_campaign_type()]
-#' @param site_code any site code. can also be a vector
+#' @param site_code any site code. Must be a single value
+#'
+#' @details This function downloads a species list and the number of times that species is observed.
+#' You must supply either the type of campaign, the code of the site, or both.
 #'
 #' @return the species list for a site, as a list
 #' @export
 get_species_list <- function(campaign_type = NULL, site_code = NULL){
+
+  # need to supply a site_code
+  assertthat::assert_that(is.character(site_code) | is.character(campaign_type))
 
   camp_type <- if ( is.null(campaign_type) ) NULL else validate_campaign_type(campaign_type)
 
--- tests/testthat/test-species_by_site.R
+++ tests/testthat/test-species_by_site.R
@@ -18,6 +18,20 @@ test_that("species_by_site returns list", {
 
 })
 
+test_that("get_species_list works for one NULL or another, but not both", {
+  resp_veg <- get_species_list("v")
+  expect_gt(nrow(resp_veg), 100)
+
+
+  resp_onesite <- get_species_list(site_code = "148_101_H01")
+
+  # greather than 10, arbitrary number
+  expect_gt(nrow(resp_onesite), 10)
+
+  expect_error(get_species_list(campaign_type = NULL, site_code = NULL))
+
+})
+
 
 test_that("can get a binary sxs matrix", {
 
