0e03bb24d243b29de352765e45fa2ae6588475c7
Guillaume Larocque <glaroc@gmail.com>
2021-07-13 12:04:11 -0400

    Add mean to richness endpoint

--- R/GET_richness.R
+++ R/GET_richness.R
@@ -11,31 +11,36 @@
 #' @param by_campaign_type whether to calculature richness by campaign type
 #' @param by_species_category whether to calculature richness by species_category
 #' @param by_date whether to calculature richness by date
+#' @param mean whether to return the mean richness for campaigns across a given campaign type or site type
 #'
 #' @return data.frame with richness values. Richness is simply calculated as unique species names that are not in category "autres".
 #' @export
 get_richness <- function(campaign_type=NULL, site_type=NULL, site_code=NULL, species_category=NULL, by_site=NULL, by_site_type=NULL, by_campaign_type=NULL, by_date=NULL, by_species_category=NULL) {
 
-  camp_type <- if ( is.null(campaign_type) ) NULL else validate_campaign_type(campaign_type)
+  if(is.null(mean)){
+    camp_type <- if ( is.null(campaign_type) ) NULL else validate_campaign_type(campaign_type)
 
-  site_type <- if ( is.null(site_type) ) NULL else validate_site_type(site_type)
+    site_type <- if ( is.null(site_type) ) NULL else validate_site_type(site_type)
 
-  params <-  list(campaign_type = camp_type,
-	          	site_type = site_type,
-	          	site_code = site_code,
-              species_category = species_category,
-              by_site = by_site,
-              by_site_type = by_site_type,
-	          	by_campaign_type = by_campaign_type,
-              by_date = by_date,
-              by_species_category = by_species_category
-          	)
-  params <- params[lapply(params,function(t){!is.null(t)}) == TRUE]
+    params <-  list(campaign_type = camp_type,
+  	          	site_type = site_type,
+  	          	site_code = site_code,
+                species_category = species_category,
+                by_site = by_site,
+                by_site_type = by_site_type,
+  	          	by_campaign_type = by_campaign_type,
+                by_date = by_date,
+                by_species_category = by_species_category
+            	)
+    params <- params[lapply(params,function(t){!is.null(t)}) == TRUE]
 
-  # function to query desired endpoing and campaign type
-  query_resp <- query_gen("richness",
-                          params)
-
+    # function to query desired endpoing and campaign type
+    query_resp <- query_gen("richness",
+                            params)
+  }else{
+    query_resp <- query_gen("richness/mean",
+                            list())
+  }
   return(query_resp)
 
 }
