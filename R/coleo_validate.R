13199fbb1e1b36c52faeb0f75362e0509a54be67
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2022-02-03 15:29:11 -0500

    short notes (and delete browser)

--- R/coleo_validate.R
+++ R/coleo_validate.R
@@ -7,5 +7,24 @@
 #' @export
 #'
 coleo_validate <- function(data, campaign_type) {
+
+  assertthat::assert_that(assertthat::has_name(data, "site_code"))
+
+
   # function to check the campaigns!
+
+  req_tables <- coleo_return_required_tables("insectes_sol")
+
+  # get the columns from those tables
+
+  #yyyy-mm-dd
+
+  stop("wrong name")
+
+  warning("")
+
+  setdiff(correct_names, injection_data_names)
+
+  setdiff(c("a", "n", "d"), letters)
+
 }
