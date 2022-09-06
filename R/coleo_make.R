2d52e123d6f8395839120a35a7c4cc7ecde07af0
vcameron1 <54558022+vcameron1@users.noreply.github.com>
2022-09-06 16:27:46 -0400

    update documentation

--- R/coleo_make.R
+++ R/coleo_make.R
@@ -1,5 +1,7 @@
 
-#'  Fonction pour standardiser la production les noms de colonnes des jeux de données à importer
+#'  Fonction pour standardiser la production des noms de colonnes des jeux de données à importer.
+#' 
+#' Les noms de colonne sont en 'snake_case' pour en faciliter la tranformation par le système interne.
 #'
 #'
 #' @param db_table une table de la base de données.
@@ -11,5 +13,3 @@ coleo_make_df_column_names <- function(db_table, db_colname) {
   valid_col_names <- paste(db_table, db_colname, sep = "_")
   return(valid_col_names)
 }
-
-
