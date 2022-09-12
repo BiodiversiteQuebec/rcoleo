f5c0943c06566efaff3134bf4bb2833ec200afe5
vcameron1 <54558022+vcameron1@users.noreply.github.com>
2022-09-12 15:41:30 -0400

    clean cell script

--- R/coleo_cell.R
+++ R/coleo_cell.R
@@ -2,22 +2,6 @@
 # being injected. These functions make those checks. coleo_cell_df will also
 # return a data.frame for a specific cell
 
-
-coleo_cell_response_format <- function(coleo_resp) {
-  #take a coleo response for a cell and return a nice data.frame
-}
-
-coleo_cell_df <- function(cell_code) {
-  #with a cell code, return a df with all cell info: name, geom
-}
-
-coleo_cell_validate <- function(df) {
-  # take a df containing some cells to validate. check the data
-  # cell_code correct # characters, one underscore
-  # name -- no NA
-  # geom is a nested list, of the kind make by coleo_cell_geom_fmt
-}
-
 #' Convert cell from sf to list
 #'
 #' We need to inject a nested list structure into the database. When we read
@@ -48,10 +32,3 @@ coleo_cell_geom_fmt <- function(cell_geom){
 }
 
 # cell injections are done with coleo_inject_general
-
-
-  # should work on the whole dataframe or just on one column?
-
-  # My instinct is that it's clearer to use functions that work on one row at a
-  # time, rather than on the whole data.frame at a time. If something like that can be worked out,
-
