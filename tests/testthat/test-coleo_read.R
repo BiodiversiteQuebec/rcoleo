3b2704133ce947cb20e6b1af9d9043f7da55fa09
vcameron1 <54558022+vcameron1@users.noreply.github.com>
2022-10-12 11:09:04 -0400

    ✅ test: read + format cell shapefile

--- tests/testthat/test-coleo_read.R
+++ tests/testthat/test-coleo_read.R
@@ -17,22 +17,37 @@ test_that("coleo_read_data returns a data.frame from a file local path", {
         ),
         fileNameXl,
         sheetName = "6. Template de téléversement")
+    # Save dummy shp file
+    fileNameShp <- paste0(tempdir(), "test.shp")
+    file.remove(fileNameShp)
+    sf::st_write(
+        data.frame(
+            cell_code = "111_111",
+            name = "Test",
+            geom = sf::st_sfc(sf::st_point(c(-68.784529, 48.112211)))
+        ),
+        dsn = fileNameShp,
+        driver = "ESRI Shapefile"
+    )
 
     # Test that the file is read without error
-    testthat::expect_error(dataCsv <- coleo_read_data(fileNameCsv), NA)
-    testthat::expect_error(dataXl <- coleo_read_data(fileNameXl), NA)
+    testthat::expect_error(dataCsv <- coleo_read(fileNameCsv), NA)
+    testthat::expect_error(dataXl <- coleo_read(fileNameXl), NA)
+    testthat::expect_error(dataShp <- coleo_read(fileNameShp), NA)
 
-    # Test that coleo_read_data reads the csv and xlsx files and returns a data.frame
+    # Test that coleo_read_data reads the files and returns a data.frame
     testthat::expect_s3_class(dataCsv, "data.frame")
     testthat::expect_s3_class(dataXl, "data.frame")
+    testthat::expect_s3_class(dataShp, c("data.frame", "sf"))
 
     # Test that coleo_read_data returns the right columns
     colNames <- c("campaigns_type", "campaigns_technicians", "efforts_photo_count")
     testthat::expect_identical(names(dataCsv), colNames)
     testthat::expect_identical(names(dataXl), colNames)
+    testthat::expect_identical(names(dataShp), c("cell_code", "name", "geom"))
 
-    # Remove dummy csv file
-    remove(list = c("fileNameCsv", "fileNameXl"))
+    # Remove dummy files
+    remove(list = c("fileNameCsv", "fileNameXl", "fileNameShp"))
 })
 
 
@@ -75,11 +90,40 @@ test_that("coleo_read_template returns a data.frame from a file local path", {
     # Test that the file is read without error
     testthat::expect_error(data <- coleo_read_template(fileName), NA)
 
-    # Test that coleo_read_csv reads the csv file and returns a data.frame
+    # Test that coleo_read_template reads the template file and returns a data.frame
     testthat::expect_s3_class(data, "data.frame")
 
     # The formating of columns is assumed by `coleo_format()`
 
-    # Remove dummy csv file
+    # Remove dummy template file
+    remove(fileName)
+})
+
+
+
+# Test that coleo_read_shape returns a data.frame from a shp file local path
+test_that("coleo_read_template returns a data.frame from a file local path", {
+    # Save dummy xlsx file
+    fileName <- paste0(tempdir(), "test2.shp")
+    file.remove(fileName)
+    sf::st_write(
+        data.frame(
+            cell_code = "111_111",
+            name = "Test",
+            geom = sf::st_sfc(sf::st_point(c(-68.784529, 48.112211)))
+        ),
+        dsn = fileName,
+        driver = "ESRI Shapefile"
+    )
+
+    # Test that the file is read without error
+    testthat::expect_error(data <- coleo_read_shape(fileName), NA)
+
+    # Test that coleo_read_template reads the template file and returns a data.frame
+    testthat::expect_s3_class(data, c("data.frame", "sf"))
+
+    # The formating of columns is assumed by `coleo_format()`
+
+    # Remove dummy template file
     remove(fileName)
 })
