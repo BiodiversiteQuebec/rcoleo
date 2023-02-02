724cebfeb001614b28b0dd3f77cf52de1679cd69
vcameron1 <victor.cameron@usherbrooke.ca>
2023-02-02 17:00:40 -0500

    accept multipolygons (1)

--- R/coleo_inject.R
+++ R/coleo_inject.R
@@ -284,7 +284,7 @@ coleo_inject <- function(df, media_path = NULL) {
   # Cells
   # - Only cells have a geom column
   if ("geom" %in% colnames(df)) {
-    if ("sfc_POLYGON" %in% class(df$geom)) {
+    if ("sfc_POLYGON" %in% class(df$geom) | "sfc_MULTIPOLYGON" %in% class(df$geom)) {
       df <- df |>
         dplyr::rowwise() |>
         dplyr::mutate(geom = list(coleo_cell_geom_fmt(geom))) |>
