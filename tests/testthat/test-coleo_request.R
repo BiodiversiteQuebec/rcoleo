21d27d35825d5ee9b66ebb997631193e51f8ef04
vcameron1 <victor.cameron@usherbrooke.ca>
2025-05-07 11:35:15 -0400

    ✅ update tests

--- tests/testthat/test-coleo_request.R
+++ tests/testthat/test-coleo_request.R
@@ -16,19 +16,18 @@ test_that("no answer for a nonsense code", {
 
 # query for a real site
 test_that("returns answer for a real code",{
-  real_site <- coleo_request_general(endpoint = "cells", schema = "public", "cell_code" = "eq.139_87")
-  answer <- httr2::resp_body_json(real_site)[[1]]
-  expect_equal(length(answer), 6)
+  answer <- coleo_request_general(endpoint = "cells", schema = "public", "cell_code" = "eq.139_87")
+  expect_equal(length(answer[[1]]), 6)
 })
 
 
 real_cell <- coleo_request_general(endpoint = "cells", schema = "public", "cell_code" = "eq.139_87")
 real_cell_df <- coleo_request_general(endpoint = "cells", schema = "public", "cell_code" = "eq.139_87", response_as_df = TRUE)
-resp_body <- httr2::resp_body_json(real_cell)[[1]]
+resp_body <- real_cell[[1]]
 test_that("response is in expected format", {
   expect_equal(length(resp_body), 6)
   # can pluck one id successfully from site request
-  expect_equal(coleo_extract_id(real_cell), 161)
+  expect_equal(resp_body$id, 161)
   expect_type(real_cell_df, "list")
 })
 
@@ -39,12 +38,12 @@ test_that("errors for bad endpoint", {
 # Site id download and extraction
 real_site <- coleo_request_general(endpoint = "sites", schema = "public", "site_code" = "eq.139_87_F01")
 test_that("request_general works for a valid site", {
-  resp_body <- httr2::resp_body_json(real_site)[[1]]
+  resp_body <- real_site[[1]]
   # should be 11 columns of info for this site
   expect_equal(length(names(resp_body)), 12)
 })
-test_that("can coleo_pluck_id correctly", {
-  expect_equal(coleo_extract_id(real_site), 94)
+test_that("can extract id correctly", {
+  expect_equal(resp_body$id, 94)
 })
 
 # Site processing works correctly
@@ -73,76 +72,15 @@ test_that("coleo_request_data returns expected output", {
   # Add more test cases for other survey types and views...
 })
 
-# with_mock_dir("coleo_request_by_code", {
-#   # request ALL the cells
-#   all_cells <- coleo_request_by_code(human_code = NULL, table = "cells")
-#   all_cells_list <- all_cells |> httr2::resp_body_json()
-#   expect_(50L, all_cells_list |> length())
-# })
+test_that("coleo_request_data handles errors correctly", {
+  # Test case with an invalid survey_type
+  expect_error(coleo_request_data(survey_type = 'invalid_survey_type'))
+})
 
-
-
-# is any of this needed?
-
-if (FALSE) {
-
-
-
-
-
-
-
-
-
-
-
-
-  resp |>
-    httr2::resp_body_json(.) |>
-    tibble::tibble(data = .) |>
-    dplyr::mutate(data = list(head(data[[1]], 5))) |>
-    tidyr::unnest_wider(data,
-                        ptype = c(id = integer(0),
-                                  cell_id = integer(0),
-                                  off_station_code_id = vctrs::unspecified(),
-                                  site_code = character(0),
-                                  type = character(0),
-                                  opened_at = character(0),
-                                  geom = "named list",
-                                  created_at = character(0),
-                                  updated_at = character(0),
-                                  cellId = integer(0),
-                                  campaigns = list(0),
-                                  cell = list(0)))
-
-
-
-  tidyr::hoist(cell, cell_namecell_name  = "name")
-
-  if (any("cell" == names(resp_no_null))) {
-    resp_no_null$cell_namecell_name  <- resp_no_null$cell$name
-    resp_no_null[["cell"]] <- NULL
-    # make it into a tibble and put it in a list of length 1
-    # resp_no_null$cell<- list(tibble::as_tibble(resp_no_null$cell))
-  }
-
-  df_for_all <- resp_no_null |>
-    purrr::map_if(~length(.)>1, ~ list(tibble::tibble(data = .))) |>
-    tibble::as_tibble()
-
-  df_for_all$campaigns[[1]] |> tidyr::unnest_wider(data)
-
-
-  site_campaign_data <- df_for_all |>
-    tidyr::unnest(campaigns) |>
-    tidyr::hoist(data,
-                 campaign_id = "id",
-                 campaign_type = "type") |>
-    dplyr::rename(campaign_data = data)
-
-  site_campaign_data
-
-
-  old_style <- get_sites(site_code='137_107_H02')
-  old_style[[1]]$body[[1]]$campaigns[[1]]
-}
+test_that("coleo_request_general handles absence of data correctly", {
+  # Test case with a non-existent endpoint
+  expect_error(coleo_request_general(endpoint = "non_existent_endpoint"))
+  # Test with existing endpoint but no data
+  expect_length(coleo_request_general(endpoint = "cells", schema = "public", 'cell_code' = "eq.non_existent_code"), 1)
+  expect_equal(coleo_request_general(endpoint = "cells", schema = "public", 'cell_code' = "eq.non_existent_code")[[1]], list())
+})
